b227bd2594bc70d72c81ef12a47c53e7c7022f41
danzel <savostyanov19d@mail.ru>
2026-03-03 16:44:05 +0200

    add explanations

--- Library-application/core/models/db_helper.py
+++ Library-application/core/models/db_helper.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.asyncio import (
 from core.config import settings
 
 
-#Класс для настройки базы данных
+# Класс для настройки базы данных
 class DatabaseHelper:
     def __init__(
         self,
@@ -20,7 +20,7 @@ class DatabaseHelper:
         pool_size: int = 5,
         max_overflow: int = 10,
     ) -> None:
-	    #Создание асинхронного движка для работы с БД
+        # Создание асинхронного движка для работы с БД
         self.engine: AsyncEngine = create_async_engine(
             url=url,
             echo=echo,
@@ -28,26 +28,29 @@ class DatabaseHelper:
             pool_size=pool_size,
             max_overflow=max_overflow,
         )
-	    #Создание асинхронных сессий
+        # Создание асинхронных сессий
         self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False,
         )
-	#Функция для закрытия соединения с БД
-	async def dispose(self) -> None:
-		await self.engine.dispose()
 
-	async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
-		async with self.sesion_factory() as session:
-			yield session
+    # Функция для закрытия соединения с БД
+    async def dispose(self) -> None:
+        await self.engine.dispose()
 
-#Инициализация заданных настроек для дальнейшего использования
+    # Цикл жизни приложения
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.sesion_factory() as session:
+            yield session
+
+
+# Инициализация заданных настроек для дальнейшего использования
 db_helper = DatabaseHelper(
-	url=str(settings.db.url),
-	echo= settings.db.echo,
-	echo_pool= settings.db.echo_pool,
-	pool_size=settings.db.pool_size,
-	max_overflow=settings.db.max_overflow,
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
 )
