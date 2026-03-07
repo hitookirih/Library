46770e644b1d194afb4da4ec04d039360c343d49
danzel <savostyanov19d@mail.ru>
2026-03-07 14:11:46 +0200

    Create workers table

--- Library-application/core/models/__init__.py
+++ Library-application/core/models/__init__.py
@@ -1,5 +1,11 @@
-__all__ = ("db_helper", "Base", "Student")
+__all__ = (
+    "db_helper",
+    "Base",
+    "Student",
+    "Worker",
+)
 
 from .base import Base
 from .db_helper import db_helper
 from .student import Student
+from .workers import Worker
--- Library-application/core/models/workers.py
+++ Library-application/core/models/workers.py
@@ -6,6 +6,7 @@ from .base import Base
 
 class Worker(Base):
 
-    fullname: Mapped[str] = mapped_column(Unique=False)
-    phone_number: Mapped[int] = mapped_column(Unique=False)
-    telegram_id: Mapped[int] = mapped_column(Unique=False)
+    fullname: Mapped[str] = mapped_column(unique=False)
+    phone_number: Mapped[int] = mapped_column(unique=False)
+    telegram_id: Mapped[int] = mapped_column(unique=False, nullable=True)
+    status: Mapped[str] = mapped_column(unique=False)
