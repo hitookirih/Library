db4ec3d068f4ef91005ca1d1cc66ec71555f2ea0
danzel <savostyanov19d@mail.ru>
2026-03-07 15:29:13 +0200

    Create covers table

---
+++ Library-application/core/models/covers.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, DATETIME, ForeignKey
+from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import mapped_column
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from .books import Book
+
+
+class Cover(Base):
+
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"))
+    filename: Mapped[str] = mapped_column(String(255))
+    storage_key: Mapped[str] = mapped_column(String(512))
+    content_type: Mapped[str] = mapped_column(String(100))
+    size: Mapped[int]
+
+    book: Mapped["Book"] = relationship(back_populates="cover")
