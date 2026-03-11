bf11cf62db4a6344fa14d4bebf670253d8955ea6
danzel <savostyanov19d@mail.ru>
2026-03-11 14:23:19 +0200

    create student by id view func

--- Library-application/api/api_v1/students.py
+++ Library-application/api/api_v1/students.py
@@ -3,9 +3,10 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper
+from core.models import db_helper, Student
 from core.schemas.student import StudentRead, StudentCreate
 from crud import students as students_crud
+from crud.students import student_by_id
 
 router = APIRouter(tags=["Students"])
 
@@ -19,6 +20,13 @@ async def get_students(
 ):
     students = await students_crud.get_all_students(session=session)
     return students
+
+
+@router.get("{student_id}", response_model=StudentRead)
+async def get_student_by_id(
+    student: Student = Depends(student_by_id),
+) -> Student:
+    return student
 
 
 @router.post("", response_model=StudentRead)
--- Library-application/crud/students.py
+++ Library-application/crud/students.py
@@ -1,10 +1,20 @@
-from typing import Sequence
+from typing import Sequence, Annotated
 
+from fastapi import Path, HTTPException, status, Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import Student
-from core.schemas.student import StudentCreate, StudentUpdatePartial, StudentUpdate
+from core.models import Student, db_helper
+from core.schemas.student import (
+    StudentCreate,
+    StudentUpdatePartial,
+    StudentUpdate,
+    StudentRead,
+)
+
+
+async def get_student(session: AsyncSession, student_id: int) -> Student | None:
+    return await session.get(Student, student_id)
 
 
 async def get_all_students(
@@ -13,6 +23,18 @@ async def get_all_students(
     stmt = select(Student).order_by(Student.id)
     result = await session.scalars(stmt)
     return result.all()
+
+
+async def student_by_id(
+    student_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.session_getter),
+) -> Student | None:
+    student = await get_student(session=session, student_id=student_id)
+    if student is not None:
+        return student
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Student {student_id} not found!"
+    )
 
 
 async def create_student(
