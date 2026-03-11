0030d63d1b40af2cc903dc5041d709313cb69ee4
danzel <savostyanov19d@mail.ru>
2026-03-11 11:44:55 +0200

    create update and delete

--- Library-application/core/schemas/student.py
+++ Library-application/core/schemas/student.py
@@ -16,5 +16,20 @@ class StudentCreate(StudentBase):
     pass
 
 
+class StudentUpdate(StudentCreate):
+    pass
+
+
+class StudentUpdatePartial(StudentCreate):
+    number_of_studak: int | None = None
+    number_of_group: str | None = None
+    name: str | None = None
+    surname: str | None = None
+    father_name: str | None = None
+    phone_number: int | None = None
+    telegram_id: str | None = None
+    status: str | None = None
+
+
 class StudentRead(StudentBase):
     id: int
--- Library-application/crud/students.py
+++ Library-application/crud/students.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Student
-from core.schemas.student import StudentCreate
+from core.schemas.student import StudentCreate, StudentUpdatePartial, StudentUpdate
 
 
 async def get_all_students(
@@ -24,3 +24,20 @@ async def create_student(
     await session.commit()
     await session.refresh(student)
     return student
+
+
+async def update_student(
+    session: AsyncSession,
+    student: Student,
+    student_update: StudentUpdate | StudentUpdatePartial,
+    partial: bool = False,
+) -> Student:
+    for name, value in student_update.model_dump(exclude_unset=partial).items():
+        setattr(student, name, value)
+        await session.commit()
+    return student
+
+
+async def delete_student(session: AsyncSession, student: Student) -> None:
+    await session.delete(student)
+    await session.commit()
