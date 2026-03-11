5d2c0af22a749534850896b9de2931ef367febaa
danzel <savostyanov19d@mail.ru>
2026-03-11 16:08:02 +0200

    add create method for books

--- Library-application/api/api_v1/books.py
+++ Library-application/api/api_v1/books.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
-from core.schemas.book import BookRead
+from core.schemas.book import BookRead, BookCreate
 from crud import books as books_crud
 
 router = APIRouter(tags=["Books"])
@@ -19,3 +19,18 @@ async def get_books(
 ):
     books = await books_crud.get_all_books(session=session)
     return books
+
+
+@router.post("", response_model=BookRead, status_code=status.HTTP_201_CREATED)
+async def create_book(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    book_create: BookCreate,
+):
+    book = await books_crud.create_book(
+        session=session,
+        book_create=book_create,
+    )
+    return book
--- Library-application/core/schemas/book.py
+++ Library-application/core/schemas/book.py
@@ -2,15 +2,16 @@ from pydantic import BaseModel
 
 
 class BookBase(BaseModel):
-
     title: str
     author: str
-    description: str
-
-    cover: str
+    description: str | None = None
 
 
 class BookCreate(BookBase):
+    pass
+
+
+class BookUpdate(BookBase):
     pass
 
 
